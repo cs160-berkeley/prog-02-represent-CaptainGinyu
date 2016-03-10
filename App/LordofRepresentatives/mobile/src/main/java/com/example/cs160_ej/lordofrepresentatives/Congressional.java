12b144c6f439f1ee84cdafab89897434841ccfd2
CaptainGinyu <kvn_chow@yahoo.com>
2016-03-10 06:17:39 -0800

    Put in code from Location API example

--- App/LordofRepresentatives/mobile/src/main/java/com/example/cs160_ej/lordofrepresentatives/Congressional.java
+++ App/LordofRepresentatives/mobile/src/main/java/com/example/cs160_ej/lordofrepresentatives/Congressional.java
@@ -1,7 +1,11 @@
 package com.example.cs160_ej.lordofrepresentatives;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentTransaction;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -13,10 +17,18 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationServices;
 
 import java.util.ArrayList;
 
-public class Congressional extends AppCompatActivity
+public class Congressional extends AppCompatActivity implements ConnectionCallbacks,
+        OnConnectionFailedListener
 {
     protected String toAppend;
     protected int receivedZipCode;
@@ -29,7 +41,18 @@ public class Congressional extends AppCompatActivity
 
     protected int currRepIndex;
 
+    /**
+     * Provides the entry point to Google Play services.
+     */
+    protected GoogleApiClient mGoogleApiClient;
+
+    /**
+     * Represents a geographical location.
+     */
+    protected Location mLastLocation;
+
     public static ArrayList<RepresentativeInfo> dummyRepInfo;
+
     static
     {
         setUpDummyRepInfo();
@@ -73,6 +96,35 @@ public class Congressional extends AppCompatActivity
         dummyRepInfo.add(rep1);
         dummyRepInfo.add(rep2);
         dummyRepInfo.add(rep3);
+    }
+
+    /**
+     * Builds a GoogleApiClient. Uses the addApi() method to request the LocationServices API.
+     */
+    protected synchronized void buildGoogleApiClient()
+    {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        if (mGoogleApiClient.isConnected())
+        {
+            mGoogleApiClient.disconnect();
+        }
     }
 
     @Override
@@ -151,6 +203,8 @@ public class Congressional extends AppCompatActivity
                 return true;
             }
         });
+
+        buildGoogleApiClient();
     }
 
     protected void updateRepInfo()
@@ -204,12 +258,64 @@ public class Congressional extends AppCompatActivity
         if (motionEvent.getAction() == MotionEvent.ACTION_DOWN)
         {
             ((Button) view).setBackgroundColor(Color.GREEN);
-        }
-        else
+        } else
         {
             ((Button) view).setBackgroundColor(origColor);
         }
 
         return true;
+    }
+
+    /**
+     * Runs when a GoogleApiClient object successfully connects.
+     */
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        // Provides a simple way of getting a device's location and is well suited for
+        // applications that do not require a fine-grained location and that do not need location
+        // updates. Gets the best and most recent location currently available, which may be null
+        // in rare cases when a location is not available.
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null)
+        {
+            Toast.makeText(this, Double.toString(mLastLocation.getLatitude()) + ", "
+                    + Double.toString(mLastLocation.getLongitude()), Toast.LENGTH_LONG).show();
+        } else
+        {
+            Toast.makeText(this, "Could not detect location", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result)
+    {
+        // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
+        // onConnectionFailed.
+        Log.i("Congressional", "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+    }
+
+
+    @Override
+    public void onConnectionSuspended(int cause)
+    {
+        // The connection to Google Play services was lost for some reason. We call connect() to
+        // attempt to re-establish the connection.
+        Log.i("Congressional", "Connection suspended");
+        mGoogleApiClient.connect();
     }
 }
